86b4f8846013087da3efcfb548825bb386204580
ArdKr <mkras01@gmx.de>
2020-12-13 18:35:28 +0100

    Add CSS Container

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import JobCard from "./components/JobCard/JobCard";
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <h1>App</h1>
       <JobCard
         companyLogo="https://www.designhill.com/resize_img.php?atyp=page_file&pth=ft_ca_ct||117||contestfile_1&flp=1554116576-13511971185ca1efe0bcd5a0-26602492.jpg"
